8053434c0cca1fb936ebc3fd32ffe14afe1775d3
Mario Suárez <masues@live.com.mx>
2022-12-24 15:10:24 -0600

    Add functionality to add a task
    Add fucntionality to add a task and render it in the todo list
    container.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import './App.css';
 import fccLogo from './imgs/freecodecamp-logo.png';
 import Task from './components/Task';
 import TaskForm from './components/TaskForm';
+import React, {useState} from 'react';
 
 function App() {
+  const [taskList,setTaskList] = useState([]);
+
+  const addTask = (task) => {
+    setTaskList((oldTaskList) => [task,...oldTaskList]);
+  };
+
   return (
     <div className='App'>
       <div className='logo-container'>
@@ -11,9 +18,14 @@ function App() {
       </div>
       <h1>Lista de tareas</h1>
       <div className='todo-list-container'>
-        <TaskForm />
+        <TaskForm addTask={addTask}/>
         <Task text='Hello World!' isCompleted={false}/>
         <Task text='This task is completed' isCompleted={true}/>
+        {
+          taskList.map( (task) =>
+            <Task key={task.id} text={task.text} isCompleted={task.completed} />
+          )
+        }
       </div>
     </div>
   );
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import '../css/TaskForm.css';
+import { v4 as uuidv4 } from 'uuid';
 
-function TaskForm(){
+function TaskForm({addTask}){
+  const handleSubmit = (event) => {
+    // Prevent to reload the page
+    event.preventDefault();
+    // Access to the content using HTMLFormElement with name='task-input-text'
+    const taskText = event.target.elements['task-input-text'].value;
+    const newTask = {
+      id: uuidv4(),
+      text: taskText,
+      completed: false
+    };
+    addTask(newTask);
+  };
+
   return (
-    <form className='task-form'>
+    <form className='task-form' onSubmit={handleSubmit}>
       <input
         className='task-input'
         type='text'
+        name='task-input-text'
         placeholder='Escribe una tarea' />
       <button className='task-button'>Agregar tarea</button>
     </form>
